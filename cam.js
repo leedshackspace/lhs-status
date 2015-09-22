9fd32b1eae8c7514882117c1f4a3476cce5c9419
Alex Silcock <alex.silcock@bskyb.com>
2015-09-22 21:18:00 +0100

    Refactor camera controller

--- cam.js
+++ cam.js
@@ -1,32 +1,47 @@
-var cam = function(el) {
-    var cams = [
-        'http://gateway.hackspace:8082', // Cam 2, covering car park
-        'http://gateway.hackspace:8083', // Cam 3, by soldering station
-        'http://gateway.hackspace:8084', // Cam 4, internal door
-        'http://wifihifi.hackspace:8081', // Cam 5, on gateway cabinet
-        'http://wifihifi.hackspace:8082', // Cam 6, outside door
-        'http://3dprinter.hackspace:8081', // Cam 7, 3D printer camera
-    ];
-    
-    var n = 0;
-    var backoff = 0;
+function CamController(el, rotateDelay) {
+    this.element = el;
+    this.rotateDelay = rotateDelay;
 
-    function nextCam() {
-        n = (n+1) % cams.length;
-        var img = new Image();
-        img.onload = function() {
-	    console.log("success");
-            el.replaceChild(img, el.querySelector('img'));
-        };
-        img.onerror = function() {
-            console.error("Camera failed to load: " + img.src);
-	    setTimeout(nextCam, backoff);
-	    backoff += 1000;
-        };
+    this.cams = {
+        'carpark': {title: 'Car park', url: 'http://gateway.hackspace:8082'},
+        'main1': {title: 'Main room 1', url: 'http://gateway.hackspace:8083'}, // by soldering station
+        'door': {title: 'Internal door', url: 'http://gateway.hackspace:8084'},
+        'main2': {title: 'Main room 2', url: 'http://wifihifi.hackspace:8081'}, // on gateway cabinet
+        'outside': {title: 'Outside door', url: 'http://wifihifi.hackspace:8082'},
+        '3dprinter': {title: '3D printer', url: 'http://3dprinter.hackspace:8081'}
+    };
 
-        img.src = cams[n];
-    }
+    this.rotateCams = ['carpark', 'outside', '3dprinter'];
+    this.currentCam = 0;
 
-    setInterval(nextCam, 15000);
-    nextCam();
+    this.nextCam();
 };
+
+CamController.prototype.nextCam = function() {
+    this.currentCam = (this.currentCam+1) % this.rotateCams.length;
+    console.log('Rotating to cam ' + this.cams[this.rotateCams[this.currentCam]].title);
+
+    var img = new Image();
+    img.addEventListener('load', function() {
+        console.log('Loaded cam ' + this.cams[this.rotateCams[this.currentCam]].title);
+        this.element.replaceChild(img, this.element.querySelector('img'));
+    }.bind(this));
+
+    img.addEventListener('error', function() {
+        console.log('Camera failed to load: ' + img.src);
+        setTimeout(this.nextCam.bind(this), 1000);
+    }.bind(this));
+
+    img.src = this.cams[this.rotateCams[this.currentCam]].url;
+}
+
+CamController.prototype.startRotation = function() {
+    if (this.rotateInterval !== null) {
+        this.rotateInterval = window.setInterval(this.nextCam.bind(this), this.rotateDelay);
+    }
+}
+
+CamController.prototype.stopRotation = function() {
+    window.clearInterval(this.rotateInterval);
+    this.rotateInterval = null;
+}
