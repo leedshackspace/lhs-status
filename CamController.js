294a0289809fe2c31391f73aa14bd1fb9e0365de
Alex Silcock <alex@alexsilcock.net>
2016-05-22 01:42:10 +0100

    Move high-level camera view control methods to CamController, use timeouts instead of interval
    Using timeouts exclusively instead of intervals combined with timeouts
    simplifies error handling when a camera stream fails to load.
    
    High-level control methods (such as fixOnCam, lingerOnCam) felt better
    placed within the main CamController module as opposed to the IRC
    control module, since those methods contain business logic.

--- CamController.js
+++ CamController.js
@@ -3,6 +3,7 @@ function CamController(camElem, camNameElem, camRefElem, rotateDelay) {
     this.nameElem = camNameElem;
     this.refElem = camRefElem;
     this.rotateDelay = rotateDelay;
+    this.shouldRotate = false;
 
     this.cams = {
         'carpark': {title: 'Car park', url: 'http://gateway.hackspace:8082'},
@@ -21,27 +22,39 @@ function CamController(camElem, camNameElem, camRefElem, rotateDelay) {
 
 CamController.prototype.nextCam = function() {
     this.currentCam = (this.currentCam+1) % this.rotateCams.length;
-    this.switchTo(this.rotateCams[this.currentCam]);
+    this.switchTo(this.rotateCams[this.currentCam], function (err) {
+        if (err) {
+            this.rotateTimeout = window.setTimeout(this.nextCam.bind(this), 1000);
+        } else if (this.shouldRotate) {
+            this.rotateTimeout = window.setTimeout(this.nextCam.bind(this), this.rotateDelay);
+        }
+    }.bind(this));
 }
 
 CamController.prototype.startRotation = function() {
-    if (!this.rotateInterval) {
-        console.log('Starting camera rotation');
-        this.rotateInterval = window.setInterval(this.nextCam.bind(this), this.rotateDelay);
+    if (this.rotateTimeout) {
+        window.clearTimeout(this.rotateTimeout);
     }
+    
+    console.log('Starting camera rotation');
+    this.shouldRotate = true;
+    this.nextCam();
 }
 
 CamController.prototype.stopRotation = function() {
     console.log('Stopping camera rotation');
-    window.clearInterval(this.rotateInterval);
-    this.rotateInterval = null;
+    window.clearTimeout(this.rotateTimeout);
+    this.rotateTimeout = null;
+    this.shouldRotate = false;
 }
 
-CamController.prototype.switchTo = function(cameraName) {
+CamController.prototype.switchTo = function(cameraName, cb /* err */) {
     if (!this.cams.hasOwnProperty(cameraName)) {
         console.log('Nonexistent camera ' + cameraName);
     }
     console.log('Switching to cam ' + this.cams[cameraName].title);
+    window.clearTimeout(this.rotateTimeout);
+    this.rotateTimeout = null;
 
     var img = new Image();
     img.addEventListener('load', function() {
@@ -49,12 +62,31 @@ CamController.prototype.switchTo = function(cameraName) {
         this.imgElement.replaceChild(img, this.imgElement.querySelector('img'));
         this.nameElem.innerHTML = this.cams[cameraName].title;
         this.refElem.innerHTML = cameraName;
+
+        cb && cb(null);
     }.bind(this));
 
     img.addEventListener('error', function() {
         console.log('Camera failed to load: ' + img.src);
-        setTimeout(this.nextCam.bind(this), 1000);
+
+        cb && cb('Camera failed to load: ' + img.src);
     }.bind(this));
 
     img.src = this.cams[cameraName].url;
+}
+
+CamController.prototype.fixOnCam = function(camName) {
+    this.stopRotation();
+    this.switchTo(camName);
+}
+
+CamController.prototype.lingerOnCam = function(camName) {
+    this.stopRotation();
+    this.switchTo(camName, function (err) {
+        if (err) {
+            return this.startRotation();
+        }
+
+        this.rotateTimeout = window.setTimeout(this.startRotation.bind(this), 30000);
+    }.bind(this));
 }
--- irc/cam.js
+++ irc/cam.js
@@ -10,7 +10,7 @@ function CamIrcConnector(controller) {
             case 'fix':
                 // View a particular camera until told otherwise
                 if (args.length === 1) {
-                    this.fixCam(args[0]);
+                    this.controller.fixOnCam(args[0]);
                 }
                 break;
 
@@ -19,41 +19,17 @@ function CamIrcConnector(controller) {
             case 'switch':
                 // Switch to a particular camera temporarily, then resume rotation
                 if (args.length === 1) {
-                    this.viewCam(args[0]);
+                    this.controller.lingerOnCam(args[0]);
                 }
                 break;
 
             case 'resume':
             case 'rotate':
                 // Resume rotating through cameras
-                this.rotateCams();
+                this.controller.startRotation();
                 break;
         }
     }.bind(this));
-}
-
-CamIrcConnector.prototype.fixCam = function (camName) {
-    this.controller.stopRotation();
-    this.controller.switchTo(camName);
-}
-
-CamIrcConnector.prototype.viewCam = function (camName) {
-    if (this.restartRotationTimeout) {
-        clearTimeout(this.restartRotationTimeout);
-    }
-    this.controller.stopRotation();
-    this.controller.switchTo(camName);
-    this.restartRotationTimeout = setTimeout(function () {
-        this.restartRotationTimeout = null;
-        this.controller.startRotation();
-    }.bind(this), 30000);
-}
-
-CamIrcConnector.prototype.rotateCams = function () {
-    this.controller.startRotation();
-    if (this.restartRotationTimeout) {
-        clearTimeout(this.restartRotationTimeout);
-    }
 }
 
 new CamIrcConnector(camController);
